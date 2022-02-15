03ec0a7ed755f8ece9458b13a0e13d2b7e244154
AnnQK <annhuddy13@yandex.ru>
2022-02-15 18:51:18 +0300

    feat: add btn and tabs functionality

--- dist/script/app.js
+++ dist/script/app.js
@@ -3,3 +3,21 @@
 const tabs = document.querySelectorAll(".tabs__item");
 const tabsBtns = document.querySelectorAll(".btn");
 const btnContainer = document.querySelector(".tabs__btn-container");
+
+btnContainer.addEventListener("click", function (event) {
+  const clicked = event.target.closest(".btn");
+
+  if (!clicked) return;
+
+  tabsBtns.forEach((btn) => {
+    btn.classList.remove("btn--active");
+  });
+
+  clicked.classList.add("btn--active");
+
+  tabs.forEach((tab) => tab.classList.remove("tabs__item--active"));
+
+  document
+    .querySelector(`.tabs__item--${clicked.dataset.tab}`)
+    .classList.add("tabs__item--active");
+});
