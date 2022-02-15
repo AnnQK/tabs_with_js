999bb55a4f4988b434ceeb036ee4cecf9118146a
AnnQK <annhuddy13@yandex.ru>
2022-02-15 18:48:04 +0300

    feat: implement variables to script file

---
+++ dist/script/app.js
@@ -0,0 +1,5 @@
+"use strict";
+
+const tabs = document.querySelectorAll(".tabs__item");
+const tabsBtns = document.querySelectorAll(".btn");
+const btnContainer = document.querySelector(".tabs__btn-container");
