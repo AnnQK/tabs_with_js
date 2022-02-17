2794f5e64b95c8e5fc59228e83057f2c8db3d3c7
AnnQK <annhuddy13@yandex.ru>
2022-02-17 10:19:32 +0300

    implement slider functiontslity

--- dist/script/app.js
+++ dist/script/app.js
@@ -3,7 +3,12 @@
 const tabs = document.querySelectorAll(".tabs__item");
 const tabsBtns = document.querySelectorAll(".btn");
 const btnContainer = document.querySelector(".tabs__btn-container");
+const slider = document.querySelector(".slider");
+const slides = document.querySelectorAll(".slide");
+const btnLeft = document.querySelector(".slider__btn--left");
+const btnRight = document.querySelector(".slider__btn--right");
 
+// Tabs
 btnContainer.addEventListener("click", function (event) {
   const clicked = event.target.closest(".btn");
 
@@ -21,3 +26,32 @@ btnContainer.addEventListener("click", function (event) {
     .querySelector(`.tabs__item--${clicked.dataset.tab}`)
     .classList.add("tabs__item--active");
 });
+
+// Slider
+
+const lastSlide = slides.length - 1;
+let currentSlide = 0; // index
+
+function showSlide(slide) {
+  slides.forEach((sl, index) => {
+    sl.style.transform = `translateX(${100 * (index - slide)}%)`;
+  });
+}
+
+function nextSlide() {
+  if (currentSlide === lastSlide) {
+    currentSlide = 0;
+  } else currentSlide++;
+  showSlide(currentSlide);
+}
+
+function prevSlide() {
+  if (currentSlide === 0) {
+    currentSlide = lastSlide;
+  } else currentSlide--;
+  showSlide(currentSlide);
+}
+//
+showSlide(0);
+btnLeft.addEventListener("click", prevSlide);
+btnRight.addEventListener("click", nextSlide);
